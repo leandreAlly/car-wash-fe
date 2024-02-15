956cf93046035099637e6189e90d170a7824872b
leandre <tuyambazeleandre@gmail.com>
2024-02-15 08:40:27 +0200

    ch(setup):Add antidesign libary and primary config

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
+import { Button } from 'antd';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-black">Landing Page</h1>
+      <Button type="primary">Primary Button</Button>
+      <h2 className="bg-primary">Test primary color</h2>
     </main>
   );
 }
